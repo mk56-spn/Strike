7c6235098b9e72df5d16745eea7ba13eec693f43
mk56-spn <darioheadleysilva@gmail.com>
2023-03-06 14:57:28 +0100

    Create death objects

---
+++ osu.Game.Rulesets.Strike/Objects/DeathObject.cs
@@ -0,0 +1,8 @@
+namespace osu.Game.Rulesets.Strike.Objects;
+
+/// <summary>
+/// An object that kills you on contact
+/// </summary>
+public class DeathObject : StrikeHitObject
+{
+}
---
+++ osu.Game.Rulesets.Strike/Objects/Drawables/DrawableDeathObject.cs
@@ -0,0 +1,9 @@
+namespace osu.Game.Rulesets.Strike.Objects.Drawables;
+
+public partial class DrawableDeathObject : DrawableStrikeHitObject
+{
+    public DrawableDeathObject(DeathObject hitObject)
+        : base(hitObject)
+    {
+    }
+}
