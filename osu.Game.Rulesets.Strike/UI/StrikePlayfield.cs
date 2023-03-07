4bb39d68f004f48e4ed20a8ecfe4842fa35b8645
mk56-spn <darioheadleysilva@gmail.com>
2023-03-07 10:34:24 +0100

    Add ControllerArea.cs

--- osu.Game.Rulesets.Strike/UI/StrikePlayfield.cs
+++ osu.Game.Rulesets.Strike/UI/StrikePlayfield.cs
@@ -3,19 +3,28 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Game.Rulesets.Strike.Objects.Controller;
 using osu.Game.Rulesets.UI;
+using osuTK;
 
 namespace osu.Game.Rulesets.Strike.UI
 {
     [Cached]
     public partial class StrikePlayfield : Playfield
     {
+        public const int SIZE = 10000;
+
         [BackgroundDependencyLoader]
         private void load()
         {
             AddRangeInternal(new Drawable[]
             {
-                HitObjectContainer,
+                new ControllerArea
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Size = new Vector2(SIZE)
+                },
             });
         }
     }
